cb7b4c8a15a2894fe84668f5c8e881c824f9e9c7
zeze <zeze@kali>
2020-04-25 16:38:21 +0800

    zeze.py async

--- hw2/zeze.py
+++ hw2/zeze.py
@@ -68,7 +68,7 @@ class Attack():
     
     def sniff_packets(self):
         for v in self.victim:
-            scapy.AsynSniffer(filter='ether mac ' + self.ip_mac[v], prn=self.process_packet, iface=self.interfaces[1], store=False)
+            scapy.AsyncSniffer(filter='ether mac ' + self.ip_mac[v], prn=self.process_packet, iface=self.interfaces[1], store=False)
 
     def process_packet(self, packet):
         if packet.haslayer(HTTPRequest) and packet[HTTPRequest].Method.decode() == 'POST':
